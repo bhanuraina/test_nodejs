b72117e7f068416cb381cd604c07cb70c19b50d2
bhanuraina <bhanuraina@gmail.com>
2024-01-11 12:37:44 +0100

    Update check_job.sh

--- check_job.sh
+++ check_job.sh
@@ -7,7 +7,7 @@ WORKFLOW_FILE="ci.yml"
 
 # Check if workflow dispatch is already running
 dispatch_status=$(curl -s -H "Accept: application/vnd.github.v3+json" \
-  -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Authorization: token $github_token" \
   https://api.github.com/repos/$OWNER/$REPO/actions/workflows/$WORKFLOW_FILE/dispatches)
 echo $dispatch_status 
 if [  $(echo $dispatch_status | jq '.total_count') = 0   ]; then
